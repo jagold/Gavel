f4881312b0cd8a6e6a893a4997938f91111a869c
Jack Goldberg <jag070@bucknell.edu>
2020-05-15 14:16:26 -0400

    Initial Commit

---
+++ legal_talk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  legal_talk
+//
+//  Created by Jack on 5/15/20.
+//  Copyright © 2020 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ legal_talkTests/legal_talkTests.swift
@@ -0,0 +1,34 @@
+//
+//  legal_talkTests.swift
+//  legal_talkTests
+//
+//  Created by Jack on 5/15/20.
+//  Copyright © 2020 Jack. All rights reserved.
+//
+
+import XCTest
+@testable import legal_talk
+
+class legal_talkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
